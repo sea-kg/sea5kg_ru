6336c5c44d76eb33fe156cb133ba8f50da22116a
sea-kg <mrseakg@gamail.com>
2015-10-06 21:12:40 +0600

    updated main page

---
+++ www/js/data.js
@@ -0,0 +1,31 @@
+var data = {
+	content : {
+		text : {
+			ru : "Привет!<br><br>Мой псевдоним sea-kg.<br>Эта страница для коллекционирования моих проектов.",
+			en : "Hello!<br><br>My nick is sea-kg.<br>This is page for aggregation my projects."
+		}
+	},
+	projects : {
+		caption : {
+			ru : "Проекты",
+			en : "Projects"
+		}
+	},
+	skills : {
+		caption : {
+			ru : "Навыки",
+			en : "Skills"
+		}
+	},
+	contacts : {
+		caption : {
+			ru : "Контакты",
+			en : "Contacts"
+		},
+		values : {
+			"Skype" : "sea-kg",
+			"E-mail" : "mrseakg@gmail.com",
+			"Linkedin" : "https://www.linkedin.com/in/seakg"
+		}
+	},
+};
